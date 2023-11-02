93f6fc0673e2f130356b466e8c38e8eba5824da0
arif <arifur.tuc@gmail.com>
2023-11-02 14:36:24 +0100

    Implement ZIP creator function

--- zipper.py
+++ zipper.py
@@ -1,27 +1,33 @@
 import PySimpleGUI as sg
+from functions import make_archive
 
 # Create labels and input elements for selecting files to compress
 select_files_label = sg.Text("Select files to compress:")
 files_input = sg.Input()
-files_browse_button = sg.FilesBrowse("Browse")
+files_browse_button = sg.FilesBrowse("Browse", key="files")
 
 # Create labels and input elements for selecting the destination folder
 select_destination_label = sg.Text("Select destination folder:")
 destination_input = sg.Input()
-destination_browse_button = sg.FolderBrowse("Browse")
+destination_browse_button = sg.FolderBrowse("Browse", key="folder")
 
 # Create a "Compress" button for initiating the compression process
 compress_button = sg.Button("Compress")
 
 # Create the main user interface window
+layout = [[select_files_label, files_input, files_browse_button],
+          [select_destination_label, destination_input, destination_browse_button],
+          [compress_button]]
 window = sg.Window("File Compressor",
-                   layout=[[select_files_label, files_input, files_browse_button],
-                           [select_destination_label, destination_input, destination_browse_button],
-                           [compress_button]],
+                   layout=layout,
                    font=('Helvetica', 20))
 
 # Wait for user interactions and collect user input from the GUI window
-user_input = window.read()
+while True:
+    event, values = window.read()
+    filepaths = values["files"].split(";")
+    folder_path = values["folder"]
+    make_archive(filepaths, folder_path)
 
 # Close the GUI window after user interaction is complete
 window.close()
