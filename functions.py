34b5a947b9fd6cb59fe69113b0ce5005da9d58bf
arif <arifur.tuc@gmail.com>
2023-11-03 20:58:59 +0100

    Add ZIP extractor function

--- functions.py
+++ functions.py
@@ -14,3 +14,16 @@ def make_archive(filepaths, des_dir):
         for filepath in filepaths:
             filepath = pathlib.Path(filepath)
             archive.write(filepath, arcname=filepath.name)
+
+
+def extract_archive(filepath, des_dir):
+    """
+    Extract the contents of a ZIP archive to a specified destination directory.
+
+    :param filepath: The path to the ZIP archive that needs to be extracted.
+    :param des_dir: The destination directory where the contents will be extracted.
+    """
+    with zipfile.ZipFile(filepath, 'r') as archive:
+        archive.extractall(des_dir)
+
+
